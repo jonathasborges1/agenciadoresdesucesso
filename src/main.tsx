8a4f1a7668839f87796fc1cc07500ee2c5b63fdb
Jonathas Borges <jbc@icomp.ufam.edu.br>
2023-08-07 04:27:51 -0400

    fix: CI/CD

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,32 @@
+// import "main.css"
+// import 'typeface-montserrat'; // Importe a fonte Montserrat
+// import React from 'react'
+// // import ReactDOM from 'react-dom/client'
+
+// import App from 'app';
+
+// const container = document.getElementById("root") as HTMLElement;
+// const root = ReactDOM.createRoot(container);
+
+// root.render(
+//   <React.StrictMode>
+//     <App></App>
+//   </React.StrictMode>
+// )
+
+
 import "main.css"
-import 'typeface-montserrat'; // Importe a fonte Montserrat
+import 'typeface-montserrat';
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom'
 
 import App from 'app';
 
 const container = document.getElementById("root") as HTMLElement;
-const root = ReactDOM.createRoot(container);
 
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <App></App>
-  </React.StrictMode>
-)
+    <App />
+  </React.StrictMode>,
+  container
+);
