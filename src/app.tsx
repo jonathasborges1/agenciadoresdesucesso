90cd2aef048643fa5185ea13b4901477474c4d27
Jonathas Borges <jbc@icomp.ufam.edu.br>
2023-08-07 07:16:30 -0400

    feat: added pixel facebook - 137959149307029 - PageView

--- src/app.tsx
+++ src/app.tsx
@@ -7,6 +7,27 @@ import { AuthProvider } from '@hooks/AuthContext';
 import ThemeProvider from '@components/Theme/themeProvider';
 import RouterProvider from '@components/Routes';
 
+import ReactPixel, { AdvancedMatching } from 'react-facebook-pixel';
+
+// Configuração do pixel
+const options: AdvancedMatching = {
+   ct: '',
+   country: '',
+   db: '',
+   em: '',
+   fn: '',
+   ge: '',
+   ln: '',
+   ph: '',
+   st: '',
+   zp: ''
+};
+ 
+// Inicializa o pixel com o ID do seu pixel do Facebook
+ReactPixel.init('137959149307029', options);
+
+// Rastreia a visualização de página
+ReactPixel.pageView();
 interface Props {
    children?: React.ReactNode;
 }
